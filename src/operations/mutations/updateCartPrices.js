a97131e4e14b0801dc3d84f1084ea808133b1c50
adaezeodurukwe <adaeze.od@softcom.ng>
2020-12-04 18:26:26 +0100

    remove unnecesary code

--- src/operations/mutations/updateCartPrices.js
+++
@@ -1,9 +0,0 @@
-export default function updateCartPrices(cartItemsVar) {
-  return (products) => {
-    const cartItems = cartItemsVar();
-    cartItemsVar(cartItems.map(item => {
-      const currentProduct = products.find(product => product.id === item.id);
-      return {...item, price: currentProduct.price};
-    }));
-  };
-}
